b179e01cb65f56badb241743a749ecd2a0264272
hrishiNEU <kulkarni.hri@northeastern.edu>
2024-12-07 16:28:55 -0500

    Update ProfileSettings.tsx

--- frontend/src/pages/ProfileSettings.tsx
+++ frontend/src/pages/ProfileSettings.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import axios from "axios";
 import "../styles/profileSettings.css";
-import Navbar from "../components/Navbar";
 import { updateProfileImage } from "../redux/authSlice";
 import { SERVER_PATHNAME } from "@utils/urls";
 
