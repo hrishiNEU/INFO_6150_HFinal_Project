05d5746009c8c55f6fba253010026b525a2afa71
hrishiNEU <kulkarni.hri@northeastern.edu>
2024-12-07 15:42:39 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,5 +29,5 @@ app.use("/api/community", communityRoutes);
 app.use("/api/events", eventRoutes);
 app.use("/api", contactRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
